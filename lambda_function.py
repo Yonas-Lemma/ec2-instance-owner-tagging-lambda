a50504e7758866a7cb20269998f88aebed5db3ee
Yonas Lemma <143127905+Yonas-Lemma@users.noreply.github.com>
2026-02-01 22:19:12 +0100

    Implement Lambda function for tagging EC2 instances
    Add a Lambda function to tag EC2 instances with the owner's username.

--- lambda_function.py
+++ lambda_function.py
@@ -1,1 +1,25 @@
 
+import json
+import boto3
+
+client = boto3.client('ec2')
+
+def lambda_handler(event, context):
+    # Extract username and instance ID from the event
+    user = event['detail']['userIdentity']['username']
+    instanceid = event['detail']['responseElements']['instancesSet']['items'][0]['instanceId']
+    
+    
+    client.create_tags(
+        Resources=[
+            instanceid,  
+        ],
+        Tags=[
+            {
+                'Key': 'Owner',
+                'Value': user
+            },
+        ]
+    )
+    
+    return 
